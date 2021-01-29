9546fa980044442f5a447998cc7a8a5cda091387
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-01-29 06:09:51 +0100

    2.2 - Unit tests - removeFromCart function

--- Cart.test.js
+++ Cart.test.js
@@ -3,8 +3,10 @@ const Cart =  require('./Cart');
 
 const cart = new Cart();
 cart.addToCart('cheesecake');
+cart.removeFromCart('cheesecake');
 
-assert.deepStrictEqual(cart.items, ['cheesecake']);
+assert.deepStrictEqual(cart.items, []);
 
 console.log('The addToCart function added an item to the cart');
+console.log('The removeFromCart function removed an item to the cart');
 
