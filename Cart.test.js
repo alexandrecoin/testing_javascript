b25d0cc55f7cc67129646aaeb97d5bad21195ecc
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-01-29 06:14:45 +0100

    2.2 - Unit tests - Add Jest and refactor tests

--- Cart.test.js
+++ Cart.test.js
@@ -1,12 +1,14 @@
-const assert = require('assert');
 const Cart =  require('./Cart');
 
-const cart = new Cart();
-cart.addToCart('cheesecake');
-cart.removeFromCart('cheesecake');
+test('The addToCart function can add an item to the cart', () => {
+    const cart = new Cart();
+    cart.addToCart('cheesecake');
+    expect(cart.items).toEqual(['cheesecake']);
+});
 
-assert.deepStrictEqual(cart.items, []);
-
-console.log('The addToCart function added an item to the cart');
-console.log('The removeFromCart function removed an item to the cart');
-
+test('The removeFromCart function can remove an item to the cart', () => {
+    const cart = new Cart();
+    cart.addToCart('cheesecake');
+    cart.removeFromCart('cheesecake');
+    expect(cart.items).toEqual([]);
+});
