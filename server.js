5e622394582775d0c3d96d9c8285e23acb2ef6d7
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-02-02 13:26:05 +0100

    3.1 - E2E tests - Break down tests

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const express = require("express");
 const app = express();
 
 let carts = new Map();
-
-function resetState() {
-    carts = new Map();
-}
+let inventory = new Map();
 
 app.get('/carts/:username/items', (req, res) => {
     const userCart = carts.get(req.params.username);
@@ -14,16 +11,19 @@ app.get('/carts/:username/items', (req, res) => {
 
 app.post('/carts/:username/items/:item', (req, res) => {
     const { username, item } = req.params;
+
+    if (!inventory.get(item)) {
+        res.status(404).json({ err: 'Could not find item in inventory' });
+    }
+
+    inventory.set(item, inventory.get(item) - 1);
     const newItems = (carts.get(username) || []).concat(item);
     carts.set(username, newItems);
     res.status(201).json(newItems);
 });
 
-
-const port = 3000;
-const server = app.listen(port);
-
 module.exports = {
-    server,
-    resetState
+    app,
+    inventory,
+    carts
 };
--- server.test.js
+++ server.test.js
@@ -1,25 +1,60 @@
-const { server, resetState } = require('./server');
+const { app, inventory, carts } = require('./server');
 const request = require('supertest');
 
-
-beforeEach(() => resetState())
-afterAll(() => server.close());
+beforeEach(() => {
+  carts.clear();
+  inventory.set("cheesecake", 1)
+})
 
 // E2E tests for API
 describe('#Server', () => {
 
   test("Getting items from a non-existing cart", async () => {
-    const getItemsResponse = await request(server).get('/carts/test_user/items')
+    const getItemsResponse = await request(app).get('/carts/test_user/items')
     expect(getItemsResponse.status).toEqual(404);
   })
   
   test("Getting items from an existing cart", async () => {
-    const addItemResponse = await request(server).post(`/carts/test_user/items/cheesecake`);
+    inventory.set('cheesecake', 1);
+    const addItemResponse = await request(app).post(`/carts/test_user/items/cheesecake`);
     expect(addItemResponse.status).toEqual(201);
     expect(addItemResponse.body).toEqual(['cheesecake']);
+    expect(inventory.get('cheesecake')).toBe(0)
   
-    const getItemsResponse = await request(server).get('/carts/test_user/items');
+    const getItemsResponse = await request(app).get('/carts/test_user/items');
     expect(getItemsResponse.status).toEqual(200);
     expect(getItemsResponse.body).toEqual(['cheesecake']);
   });
+
+  test("Getting items from an empty inventory", async () => {
+    inventory.set('cheesecake', 0)
+    const addItemResponse = await request(app).post(`/carts/test_user/items/cheesecake`);
+    expect(addItemResponse.status).toEqual(404);
+  });
 });
+
+  
+describe("addItem", () => {
+
+  test("correct response", async () => {
+    const addItemResponse = await request(app).post('/carts/test_user/items/cheesecake');
+    expect(addItemResponse.status).toBe(201);
+    expect(addItemResponse.body).toEqual(["cheesecake"]);
+  });
+  test("soldout items", async () => { 
+    inventory.set("cheesecake", 0);
+    const failedAddItem = await request(app).post('/carts/test_user/items/cheesecake'); 
+    expect(failedAddItem.status).toBe(404);
+  }); 
+
+  test("inventory update", async () => {
+    await request(app).post('/carts/test_user/items/cheesecake'); 
+    expect(inventory.get("cheesecake")).toBe(0);
+  });
+
+  test("cart update", async () => {
+    await request(app).post('/carts/test_user/items/cheesecake'); 
+    expect(carts.get("test_user")).toEqual(["cheesecake"]);
+    });
+});
+
