c3a011ff5daa88bbaca90379e44e780f68584b51
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-02-11 14:06:20 +0100

    4.1.2 - Integration Testing - Test deleteItemFromCart function

--- cartController.test.js
+++ cartController.test.js
@@ -1,4 +1,4 @@
-const { carts, addItemToCart } = require('./cartController');
+const { carts, addItemToCart, deleteItemFromCart } = require('./cartController');
 const { inventory } = require('./inventoryController');
 
 afterEach(() => {
@@ -7,30 +7,54 @@ afterEach(() => {
 })
 
 describe('Integration | addItemToCart', () => {
+    describe('addItemToCart', () => {
+        test('adding unavailable items to the cart', () => {
+            inventory.set('cheesecake', 0);
+            carts.set('test_user', []);
 
-    test('adding unavailable items to the cart', () => {
-       inventory.set('cheesecake', 0);
-       carts.set('test_user', []);
+            try {
+                addItemToCart('test_user', 'cheesecake');
+            } catch(err) {
+                const expectedError = new Error('cheesecake is unavailable.');
+                expectedError.code = 404;
 
-       try {
-           addItemToCart('test_user', 'cheesecake');
-       } catch(err) {
-           const expectedError = new Error('cheesecake is unavailable.');
-           expectedError.code = 404;
+                expect(expectedError).toEqual(err);
+            }
 
-           expect(expectedError).toEqual(err);
-       }
+            expect(carts.get('test_user')).toEqual([]);
+            expect.assertions(2);
+        });
 
-       expect(carts.get('test_user')).toEqual([]);
-       expect.assertions(2);
+        test('adding available items to the cart', () => {
+            inventory.set('cheesecake', 1);
+            carts.set('test_user', []);
+
+            addItemToCart('test_user', 'cheesecake');
+            expect(carts.get('test_user')).toEqual(['cheesecake']);
+            expect(inventory.get('cheesecake')).toEqual(0);
+        });
     });
 
-    test('adding available items to the cart', () => {
-       inventory.set('cheesecake', 1);
-       carts.set('test_user', []);
+    describe('deleteItemFromCart', () => {
+        test('removing item from cart when no item', () => {
+            carts.set('test_user', []);
+            try {
+                deleteItemFromCart('test_user', 'cheesecake');
+            } catch(err) {
+                const expectedError = new Error({ message: 'An error has occured' });
+                expectedError.status = 404;
 
-       addItemToCart('test_user', 'cheesecake');
-       expect(carts.get('test_user')).toEqual(['cheesecake']);
-       expect(inventory.get('cheesecake')).toEqual(0);
+                expect(expectedError).toEqual(err);
+            }
+
+            expect(carts.get('test_user')).toEqual([]);
+        });
+
+        test('removing item from cart when item is available', () => {
+            carts.set('test_user', 'cheesecake');
+            inventory.set('cheesecake', 0);
+            deleteItemFromCart('test_user', 'cheesecake');
+            expect(carts.get('test_user')).toEqual([]);
+        });
     });
 });
