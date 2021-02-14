7470ed7bfbdc3567fdff201bc46e7dae6b3d7603
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-02-14 06:54:54 +0100

    4.2.1 - Testing middlewares - Add hasValidCredentials function

--- authenticationController.js
+++ authenticationController.js
@@ -8,4 +8,16 @@ const hashPassword = password => {
   return hash.digest("hex");
 }
 
-module.exports = { users, hashPassword };
+const areCredentialsValid = (username, password) => {
+  const userExists = users.has(username);
+  if (!userExists) return false;
+
+  const currentPasswordHash = users.get(username).passwordHash;
+  return hashPassword(password) === currentPasswordHash;
+}
+
+module.exports = {
+  users,
+  hashPassword,
+  areCredentialsValid
+};
--- authenticationController.test.js
+++ authenticationController.test.js
@@ -1,7 +1,7 @@
-const { TestScheduler } = require('jest');
 const crypto = require('crypto');
-const { hashPassword } = require('./authenticationController');
+const { hashPassword, areCredentialsValid, users } = require('./authenticationController');
 
+// Unit testing
 describe('hashPassword', () => {
   test('hashing password', () => {
     const plainTextPassword = 'azerty123';
@@ -11,5 +11,39 @@ describe('hashPassword', () => {
     const expectedHash = hash.digest("hex");
     const actualHash = hashPassword(plainTextPassword);
     expect(actualHash).toEqual(expectedHash);
+  });
+});
+
+// Integration testing : Interaction with DB
+
+beforeEach(() => users.clear());
+
+describe('areCredentialsValid', () => {
+  test('valid credentials', () => {
+    users.set('test_user', {
+      email: 'test_user@test.fr',
+      passwordHash: hashPassword('azerty123')
+    });
+
+    const hasValidCredentials = areCredentialsValid('test_user', 'azerty123');
+
+    expect(hasValidCredentials).toBe(true);
+  });
+
+  test('invalid credentials', () => {
+    users.set('test_user', {
+      email: 'test_user@test.fr',
+      passwordHash: hashPassword('azerty123')
+    });
+
+    const hasValidCredentials = areCredentialsValid('test_user', 'qsdfgh456');
+
+    expect(hasValidCredentials).toBe(false);
+  });
+
+  test('user does not exist', () => {
+    const result = areCredentialsValid('test_user', 'azerty123');
+
+    expect(result).toBe(false);
   });
 });
