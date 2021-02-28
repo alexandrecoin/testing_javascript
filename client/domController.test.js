98044cec81de909a4d89b71fa14b76aeb49ff43e
alexandrecoin <alexandre.pc.coin@gmail.com>
2021-02-28 05:36:55 +0100

    6.2.1 - dom-testing-library

--- client/domController.test.js
+++ client/domController.test.js
@@ -1,38 +1,34 @@
 const fs = require("fs");
 const { updateItemList } = require('./domController');
 const initialHtml = fs.readFileSync("./index.html");
+const { screen, getByText } = require('@testing-library/dom')
 
 beforeEach(() => document.body.innerHTML = initialHtml);
 
 describe('updateItemList', () => {
     test('it renders a list of items', () => {
        const inventory = {
-           "cheesecake": 5,
+           cheesecake: 5,
            "apple pie": 3,
         };
         updateItemList(inventory);
         const itemList = document.getElementById('item-list');
         expect(itemList.childNodes).toHaveLength(2);
 
-        // The `childNodes` property has a `length`, but it's NOT an Array
-        const nodesText = Array.from(itemList.childNodes).map(node => node.innerHTML );
-
-        expect(nodesText).toContain("cheesecake - Quantity: 5");
-        expect(nodesText).toContain("apple pie - Quantity: 3");
+        expect(getByText(itemList, "cheesecake - Quantity: 5", { selector: "li" })).toBeTruthy();
+        expect(getByText(itemList, "apple pie - Quantity: 3")).toBeTruthy();
     });
 
-    // test('it renders a paragraph indicating what was the last inventory update', () => {
-    //     const inventory = {
-    //        "cheesecake": 5,
-    //        "apple pie": 3,
-    //     };
-    //     updateItemList(inventory);
-    //     const paragraphs = Array.from(document.querySelector('p'));
-    //     const updateParagraphs = paragraphs.filter(paragraph => {
-    //         return paragraph.includes('The inventory has been updated');
-    //     });
-    //
-    //     expect(updateParagraphs).toHaveLength(1);
-    //     expect(updateParagraphs[0].innerHTML).toBe(`The inventory has been updated - ${JSON.stringify(inventory)}`);
-    // });
+
+    test("adding a paragraph indicating what was the update", () => {
+        const inventory = {
+            cheesecake: 5,
+            "apple pie": 2
+        };
+
+        updateItemList(inventory);
+
+        expect( screen.getByText(
+            `The inventory has been updated - ${JSON.stringify(inventory)}` )
+        ).toBeTruthy(); });
 });
